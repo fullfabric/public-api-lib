c7dece12f601118d45d94a6a3f20ae2c9860e362
cfscosta <carlos.fsilvacosta@gmail.com>
2023-04-20 11:30:00 +0100

    lint

--- src/api/getCascadingDropDownOptions.js
+++ src/api/getCascadingDropDownOptions.js
@@ -1,6 +1,10 @@
 import url from '../utils/url'
 
-export default async function getCascadingDropDownOptions(field, parentValue, queryOpts) {
+export default async function getCascadingDropDownOptions(
+  field,
+  parentValue,
+  queryOpts
+) {
   let path = `/api/fields/sub_fields/${field.id}/options`
   if (parentValue) {
     path += `?parent_value=${parentValue}`
