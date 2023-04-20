7fab587920d625b41c83fad9ecda15d3ba53a1d5
Carlos Costa <carlos.fsilvacosta@gmail.com>
2023-04-20 14:00:42 +0100

    #1204413098132735 - export get cascading dropdowns (#5)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as getCountries } from './api/getCountries'
+export { default as getCascadingDropDownOptions } from './api/getCascadingDropDownOptions'
 export { default as getForm } from './api/getForm'
 export { default as getPolicy } from './api/getPolicy'
 export { default as submitForm } from './api/submitForm'
