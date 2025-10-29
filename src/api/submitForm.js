7ed1859ce825c186ba4a78ef48f8d47678ef528a
João Amaral <j_amaral@live.com.pt>
2025-10-29 13:15:39 +0000

    removed anti spam filter from forms and events

--- src/api/submitForm.js
+++ src/api/submitForm.js
@@ -2,7 +2,6 @@ import assert from 'assert'
 import { pick } from 'lodash-es'
 
 import checkResponse from '../utils/checkResponse'
-import getPageDigest from '../utils/getPageDigest'
 import url from '../utils/url'
 
 /**
@@ -10,7 +9,6 @@ import url from '../utils/url'
  *
  * @param {String} formId A Form ID
  * @param {FormData} formData The data to submit to the form.
- * @param {String} [opts.requestId] Customize the requestId used for anti-spam.
  * @param {String} [opts.baseUrl] The base URL for the API. Set if different
  *                                from the current location.
  * @param {String} [opts.externalDomainToken] A token to include in the request
@@ -26,12 +24,6 @@ export default async function submitForm(formId, formData, opts = {}) {
     'formData must be provided and be a FormData instance'
   )
 
-  const digest = await getPageDigest({
-    requestId: opts.requestId,
-    sourceType: 'form',
-    sourceId: formId
-  })
-
   const urlOpts = {
     ...pick(opts, ['baseUrl']),
     query: { external_domain_token: opts.externalDomainToken }
@@ -42,7 +34,6 @@ export default async function submitForm(formId, formData, opts = {}) {
     {
       credentials: 'same-origin',
       method: 'POST',
-      headers: { 'X-FF-DIGEST': digest },
       body: formData
     }
   )
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,3 @@
-export { default as getPageDigest } from './getPageDigest'
 export { default as checkResponse } from './checkResponse'
 export { default as extractHeaders } from './extractHeaders'
 export { default as url } from './url'
