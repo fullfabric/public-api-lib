731b38d26ad1a176c736c47be25da711d52ca907
João Amaral <j_amaral@live.com.pt>
2025-10-30 10:05:40 +0000

    SQ01-204 - remove anti spam filter from forms and events fix (#15)

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+export { default as getPageDigest } from './getPageDigest'
 export { default as checkResponse } from './checkResponse'
 export { default as extractHeaders } from './extractHeaders'
 export { default as url } from './url'
